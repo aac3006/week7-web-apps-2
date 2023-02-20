7353d2188caa49ffd67b72bb1d01596cdb2bb1a2
aac3006 <alicia.alessandri@kellogg.northwestern.edu>
2023-02-20 01:57:08 +0000

    practice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["city"]
+    @post["image"] = params["post"]["image"]
+    @post.save
+    redirect_to "/posts"
+  end
+
+  def destroy
+    # find post find_by
+    # post.destroy
+    # redirect
+  end
+
 end
